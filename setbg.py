ae1fa7b9d47a1ef91a23cf1a3800bf70b18a957b
mijara <marjara35@gmail.com>
2017-05-26 21:23:03 -0400

    Move to Python

--- setbg.py
+++ setbg.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 import datetime
 import platform
 import subprocess
+import os
 
-#home=/usr/local/sadRick/images
 HOME = '/usr/local/sadRick/images/'
 THUMB = 'thumb%.3d.png'
 
@@ -10,26 +12,19 @@ def get_picture(h, m):
     return THUMB % (((h * 60 + m) / 8 + 37) % 180 + 1)
 
 time = datetime.datetime.now()
-picture = THUMB % 100 #get_picture(time.hour, time.minute)
+picture = get_picture(time.hour, time.minute)
 path = HOME + picture
 
 os_name = platform.system()
 
 if os_name == 'Darwin':
-    print subprocess.call(['osascript', '-e', '"tell application \\\"Finder\\\" to set desktop picture to POSIX file \\\"%s\\\""' % path])
+    subprocess.call(['osascript', '-e', r'tell application "Finder" to set desktop picture to POSIX file "%s"' % path])
 
-"""
-if [ "$(uname)" == "Darwin" ]; then
-	osascript -e
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export DISPLAY=:0
-  case $DESKTOP_SESSION in
-  gnome )
-      gsettings set org.gnome.desktop.background picture-uri "file://$home/thumb$(printf "%03d" $(( 10#$picture )) ).png"
-      ;;
-  * )
-      feh --bg-scale "$home/thumb$(printf "%03d" $(( 10#$picture )) ).png"
-      ;;
-  esac
-fi
-"""
+elif os_name == 'Linux':
+    subprocess.call(['export', 'DISPLAY=:0'])
+    dekstop_session = os.environ['DESKTOP_SESSION']
+
+    if dekstop_session == 'gnome':
+        subprocess.call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', 'file://' + path])
+    else:
+        subprocess.call(['feh', '--bg-scale', path])
